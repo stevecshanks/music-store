b494d0e6febba599255133005702600a96769117
Steve Shanks <steve.c.shanks@gmail.com>
2018-07-07 12:14:55 +0100

    Allow user to "buy" albums

--- store/albums.py
+++ store/albums.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template
-from store.models import Album
+from flask import Blueprint, render_template, redirect, url_for
+from store.models import Album, db
 
 bp = Blueprint('albums', __name__, url_prefix='/albums')
 
@@ -7,5 +7,19 @@ bp = Blueprint('albums', __name__, url_prefix='/albums')
 @bp.route('/')
 def index():
     albums = Album.query.all()
-
     return render_template('albums/index.html', albums=albums)
+
+
+@bp.route('/buy/<int:album_id>')
+def buy(album_id):
+    album = Album.query.get(album_id)
+    if not album:
+        raise ValueError('Album not found')
+    if album.purchased:
+        raise ValueError('You already own this album')
+
+    album.purchased = True
+    db.session.add(album)
+    db.session.commit()
+
+    return redirect(url_for('albums.index'))
--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-
+from sqlalchemy.sql import expression
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -12,3 +12,4 @@ class Album(db.Model):
     artist = db.Column(db.String(255), nullable=False)
     cover_image_url = db.Column(db.String(255), nullable=False)
     bandcamp_url = db.Column(db.String(255), nullable=False)
+    purchased = db.Column(db.Boolean(), nullable=False, server_default=expression.false())
