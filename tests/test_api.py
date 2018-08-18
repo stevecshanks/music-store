a1a04e036ea222f3acb18ef45fd2fb20327e95ce
Steve Shanks <steve.c.shanks@gmail.com>
2018-08-18 16:05:45 +0100

    Add API tests for rating album

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,4 @@
+import json
 from tests import ApiTestCase
 from flask import url_for
 from store.models import db, Album
@@ -16,6 +17,27 @@ class AlbumsTest(ApiTestCase):
 
         response = self.client.get(url_for('api.albums'))
         self.assertEqual(response.status_code, 200)
-        self.assertResponseEqualsJson(response, [
-            {'id': 1, 'artist': 'Test Artist', 'name': 'Test', 'cover_image_url': 'a url',
-             'bandcamp_url': 'another url', 'purchased': False, 'rating': album.id}])
+        self.assertResponseEqualsJson(response,
+                                      [{'id': album.id, 'artist': 'Test Artist', 'name': 'Test',
+                                        'cover_image_url': 'a url', 'bandcamp_url': 'another url', 'purchased': False,
+                                        'rating': 1}])
+
+    def test_rating_non_existent_album_returns_correct_error(self):
+        response = self.client.put(url_for('api.rate', album_id=999), data=json.dumps({'rating': 1}),
+                                   headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 500)
+        self.assertResponseEqualsJson(response, {'error': 'No album found with id 999'})
+
+    def test_rating_an_album_returns_the_updated_album(self):
+        album = Album(artist='Test Artist', name='Test', cover_image_url='a url', bandcamp_url='another url', rating=1)
+        db.session.add(album)
+        db.session.commit()
+
+        response = self.client.put(url_for('api.rate', album_id=album.id), data=json.dumps({'rating': 5}),
+                                   headers={'Content-Type': 'application/json'})
+        self.assertEqual(response.status_code, 200)
+        self.assertResponseEqualsJson(response,
+                                      {'id': album.id, 'artist': 'Test Artist', 'name': 'Test',
+                                       'cover_image_url': 'a url', 'bandcamp_url': 'another url', 'purchased': False,
+                                       'rating': 5})
+
